ff3fb7be68a71a4641a57061154d2186771ba093
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-02-15 12:44:26 -0300

    feat: fetching data with axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 
+import axios from 'axios'
+
 type Repository = {
   full_name: string
   description: string
@@ -9,9 +11,13 @@ function App() {
   const [repositories, setRepositories] = useState<Repository[]>([])
 
   useEffect(() => {
-  fetch('https://api.github.com/users/mateusjbarbosa/repos')
-    .then(res => res.json())
-    .then(data => { setRepositories(data) })
+    // fetch('https://api.github.com/users/mateusjbarbosa/repos')
+    //   .then(res => res.json())
+    //   .then(data => { setRepositories(data) })
+
+      axios
+        .get('https://api.github.com/users/mateusjbarbosa/repos')
+        .then(response => { setRepositories(response.data) })
   }, [])
 
   return (
