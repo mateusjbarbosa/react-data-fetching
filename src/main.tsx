2cbbe669116ed19d28b1205f6e5f06f271e62b3f
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-02-15 12:58:49 -0300

    feat: add react query provider in main app

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { QueryClientProvider } from 'react-query'
+
+import { queryClient } from './services/queryClient'
+
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
