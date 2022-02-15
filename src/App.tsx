1125632d2c93d48ca4902210d8d8caa1354d46b1
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-02-15 12:46:28 -0300

    feat: fetching data with custom axios hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 
-import axios from 'axios'
+// import axios from 'axios'
+
+import { useFetch } from './hooks/useFetch'
 
 type Repository = {
   full_name: string
@@ -8,21 +10,25 @@ type Repository = {
 }
 
 function App() {
-  const [repositories, setRepositories] = useState<Repository[]>([])
+  // const [repositories, setRepositories] = useState<Repository[]>([])
 
-  useEffect(() => {
-    // fetch('https://api.github.com/users/mateusjbarbosa/repos')
-    //   .then(res => res.json())
-    //   .then(data => { setRepositories(data) })
+  const { data: repositories, isFetching } =
+    useFetch<Repository[]>('users/mateusjbarbosa/repos')
 
-      axios
-        .get('https://api.github.com/users/mateusjbarbosa/repos')
-        .then(response => { setRepositories(response.data) })
-  }, [])
+  // useEffect(() => {
+  // fetch('https://api.github.com/users/mateusjbarbosa/repos')
+  //   .then(res => res.json())
+  //   .then(data => { setRepositories(data) })
+
+  // axios
+  //   .get('https://api.github.com/users/mateusjbarbosa/repos')
+  //   .then(response => { setRepositories(response.data) })
+  // }, [])
 
   return (
     <ul>
-      {repositories.map(repo => (
+      {isFetching && <p>Loading...</p>}
+      {repositories?.map(repo => (
         <li key={repo.full_name}>
           <strong>{repo.full_name}</strong>
           <p>{repo.description}</p>
