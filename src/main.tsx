b7e07ac39b5f2ff75078d705923550af9ef0676a
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-02-15 13:35:21 -0300

    feat: add browser router in main app

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
+
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import { QueryClientProvider } from 'react-query'
 
 import { queryClient } from './services/queryClient'
 
-import App from './App'
+import { App } from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
