9778dabd223e274bb0677c6a955fdb5a50f24769
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-02-15 13:35:09 -0300

    feat: create app routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,13 @@
-// import { useEffect, useState } from 'react'
+import { Route, Routes } from "react-router-dom";
 
-import axios from 'axios'
+import { Repository } from './pages/Repository'
+import { Repositories } from './pages/Repositories'
 
-import { useQuery } from "react-query"
-
-// import { useFetch } from './hooks/useFetch'
-
-type Repository = {
-  full_name: string
-  description: string
-}
-
-function App() {
-  // const [repositories, setRepositories] = useState<Repository[]>([])
-
-  // const { data: repositories, isFetching } =
-  //   useFetch<Repository[]>('users/mateusjbarbosa/repos')
-
-  const { data: repositories, isFetching } = useQuery<Repository[]>('repos', async () => {
-    const res = await axios.get('https://api.github.com/users/mateusjbarbosa/repos')
-
-    return res.data
-  })
-
-  // useEffect(() => {
-  //   fetch('https://api.github.com/users/mateusjbarbosa/repos')
-  //     .then(res => res.json())
-  //     .then(data => { setRepositories(data) })
-
-  //   axios
-  //     .get('https://api.github.com/users/mateusjbarbosa/repos')
-  //     .then(response => { setRepositories(response.data) })
-  // }, [])
-
+export function App() {
   return (
-    <ul>
-      {isFetching && <p>Loading...</p>}
-      {repositories?.map(repo => (
-        <li key={repo.full_name}>
-          <strong>{repo.full_name}</strong>
-          <p>{repo.description}</p>
-        </li>
-      ))}
-    </ul>
+    <Routes>
+      <Route path="/" element={<Repositories />} />
+      <Route path="/repository/*" element={<Repository />} />
+    </Routes>
   )
 }
-
-export default App
